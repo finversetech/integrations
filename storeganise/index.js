cbbc9f4463e59145a3d87ac132fda6ff268215e5
Pranay Agarwal <pagarwal@finverse.com>
2024-08-07 15:41:18 +0800

    Fetch credz from secret manager + cache

--- storeganise/index.js
+++ storeganise/index.js
@@ -1,4 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const { FinverseSdk } = require('./sdk/finverse');
 const { StoreganiseSdk } = require('./sdk/storeganise');
 const { finverseWebhookHandler } = require('./handler');
@@ -7,22 +8,62 @@ const { finverseWebhookHandler } = require('./handler');
 // it is okay to store this field as a run-time variable
 const storeganiseBusinessCode = process.env.storeganise_business_code;
 
-// the storeganise API key is a sensitive field that should be stored in the appropriate location for secrets
-// currently storing it as a runtime variable as part of v1. Should move to secret manager as part of official production-ization
-const storeganiseApiKey = process.env.storeganise_api_key;
+const secretNames = {
+  storeganiseApiKey: process.env.storeganise_api_key,
+  finverseClientId: process.env.finverse_client_id,
+  finverseClientSecret: process.env.finverse_client_secret,
+};
 
-// Finverse client id and secrets are sensitive fields and should be stored in the appropriate location for secrets
-// currently storing as runtime variables as part of v1. Should move to secret manager as part of official production-ization
-const finverseClientId = process.env.finverse_client_id;
-const finverseClientSecret = process.env.finverse_client_secret;
+const secretManagerClient = new SecretManagerServiceClient();
+// cache the secret values so that we don't need to fetch the values every time we get a request
+const cachedSecretValues = {
+  finverseClientId: '',
+  finverseClientSecret: '',
+  storeganiseApiKey: '',
+};
 
 // Should set the entry point in Google Cloud Functions to `storeganiseHelper` so that it uses this function
 functions.http('storeganiseHelper', async (req, res) => {
-  const finverseSdk = new FinverseSdk(finverseClientId, finverseClientSecret);
+  if (cachedSecretValues.finverseClientId === '') {
+    cachedSecretValues.finverseClientId = await readSecret(
+      secretManagerClient,
+      secretNames.finverseClientId
+    );
+  }
+  if (cachedSecretValues.finverseClientSecret === '') {
+    cachedSecretValues.finverseClientSecret = await readSecret(
+      secretManagerClient,
+      secretNames.finverseClientSecret
+    );
+  }
+  if (cachedSecretValues.storeganiseApiKey === '') {
+    cachedSecretValues.storeganiseApiKey = await readSecret(
+      secretManagerClient,
+      secretNames.storeganiseApiKey
+    );
+  }
+
+  const finverseSdk = new FinverseSdk(
+    cachedSecretValues.finverseClientId,
+    cachedSecretValues.finverseClientSecret
+  );
   const storeganiseSdk = new StoreganiseSdk(
     storeganiseBusinessCode,
-    storeganiseApiKey
+    cachedSecretValues.storeganiseApiKey
   );
 
   await finverseWebhookHandler(req, res, finverseSdk, storeganiseSdk);
 });
+
+/**
+ * Fetch the latest secret value
+ * @param {SecretManagerServiceClient} secretManagerClient
+ * @param {string} secretName
+ * @returns {string} the secret value in plaintext
+ */
+async function readSecret(secretManagerClient, secretName) {
+  const [response] = await secretManagerClient.accessSecretVersion({
+    name: secretName + '/versions/latest',
+  });
+  return response.payload?.data.toString();
+}
