ce9ad35886d509b4b724c844d0baa4f376bc0882
Pranay Agarwal <pagarwal@finverse.com>
2024-09-04 15:14:58 +0800

    clean up finverse SDK

--- storeganise/sdk/finverse.js
+++ storeganise/sdk/finverse.js
@@ -1,45 +1,3 @@
-const crypto = require('crypto');
-
-// used to verify webhook signatures. Can be retrieved from https://docs.finverse.com/#bf53157c-8de2-418f-be88-38f81332be4b
-const finversePublicKey = `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEuZId/6U0gKLodSihwC/EuMGtULx8
-G3r7X7nZ3KWO5uNVtRTC64MH/1faq9zRp/2iIjCT8erSxiyO6y8wnlqMqw==
------END PUBLIC KEY-----`;
-
-/**
- * Checks if a given JWT token is valid (i.e. it expires >5 seconds from now)
- * @param {string} token
- * @returns true if token is valid, false if it needs to be refreshed
- */
-function isTokenValid(token) {
-  if (typeof token !== 'string' || token === '') {
-    return false;
-  }
-
-  const jwtPayloadStr = token.split('.')[1];
-  if (!jwtPayloadStr) {
-    return false;
-  }
-
-  const decodedJwtPayload = (() => {
-    try {
-      return JSON.parse(atob(jwtPayloadStr));
-    } catch {
-      return null;
-    }
-  })();
-
-  if (decodedJwtPayload === null) {
-    return false;
-  }
-
-  const expiryInMs = decodedJwtPayload.exp * 1000;
-  const currentTime = new Date().getTime();
-
-  // token is valid if token is valid for >5 seconds
-  return expiryInMs - currentTime > 5000;
-}
-
 class FinverseSdk {
   /**
    * Finverse client id
@@ -53,22 +11,23 @@ class FinverseSdk {
   clientSecret;
 
   /**
-   * The finverse JWT token that is used to access the API endpoints
-   * @type string
-   */
-  token;
-
-  /**
    * @param {string} clientId
    * @param {string} clientSecret
    */
   constructor(clientId, clientSecret) {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
-    this.token = '';
   }
 
-  async fetchAndSetFinverseToken() {
+  /**
+   * Fetches a customer token
+   *
+   * Note: This token has a validitiy of 3600 seconds (= 60 mins, or 1 hour). This token can be cached and re-used.
+   * For now we are fetching it before every API call for simplicity
+   *
+   * @returns {string} The finverse customer token which can be used to access the API
+   */
+  async fetchFinverseToken() {
     const { access_token } = await fetch(
       'https://api.prod.finverse.net/auth/customer/token',
       {
@@ -87,21 +46,17 @@ class FinverseSdk {
       if (r.ok) return r.json().catch(() => null);
       throw new Error(`Finverse token error`);
     });
-    this.token = access_token;
+    return access_token;
   }
 
   async fetchFinverse(path, { method = 'GET', body, headers = {} } = {}) {
-    if (!isTokenValid(this.token)) {
-      // Get an access_token. We will use this to authenticate the request to Finverse
-      await this.fetchAndSetFinverseToken();
-      this.token = access_token;
-    }
+    const accessToken = await this.fetchFinverseToken();
 
     // generic API call
     return fetch('https://api.prod.finverse.net/' + path, {
       method,
       headers: {
-        Authorization: `Bearer ${this.token}`,
+        Authorization: `Bearer ${accessToken}`,
         ...(body && { 'Content-Type': 'application/json' }),
         ...headers,
       },
@@ -112,21 +67,6 @@ class FinverseSdk {
       const errResponse = await r.text();
       throw new Error(`Finverse error: ${errResponse}`);
     });
-  }
-
-  /**
-   * Pass in the cached token, it if is valid, we will set it on the sdk as is, else we will refresh the token
-   * @param {string} token - the cached token
-   */
-  async setCachedTokenOrRefresh(token) {
-    if (isTokenValid(token)) {
-      // token is valid
-      this.token = token;
-      return;
-    }
-
-    // token is not valid, should get new
-    await this.fetchAndSetFinverseToken();
   }
 
   /**
@@ -143,34 +83,8 @@ class FinverseSdk {
       throw err;
     }
   }
-
-  /**
-   * Verify the signature of the webhook to confirm that the sender is Finverse
-   * @param {string} payload - Raw payload
-   * @param {string} signature - Signature as found in fv-signature header
-   */
-  static verifySignature(payload, signature) {
-    // signature is encoded in base64 and should be decoded
-    const decodedSignature = Buffer.from(signature, 'base64');
-    const textEncoder = new TextEncoder();
-    // signature is created on the raw payload in bytes
-    const payloadInBytes = textEncoder.encode(payload);
-
-    const publicKey = crypto.createPublicKey(finversePublicKey);
-    const verify = crypto.createVerify('SHA256');
-    verify.update(payloadInBytes).end();
-    return verify.verify(publicKey, decodedSignature);
-  }
-
-  /**
-   * @returns {string} the finverse customer token
-   */
-  getToken() {
-    return this.token;
-  }
 }
 
 module.exports = {
   FinverseSdk,
-  isTokenValid,
 };
